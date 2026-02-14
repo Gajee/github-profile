4a863ea63462ee9f33015cc04f9c3fb63d0662b3
Gajee <mdghaziattar@gmail.com>
2026-02-14 17:23:27 +0530

    added profile image in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
+import { getUser } from "../api/github";
+
 interface Props {
   username: string;
 }
 const Header = ({ username }: Props) => {
+
+    const [user, setUser] = useState<any>(null);
+
+    useEffect(() => {
+        getUser(username).then(setUser);
+      }, [username]);
+
+      if (!user) return <div>Loading...</div>;
   return (
     <div className="bg-black border-b border-githubBorder px-10 py-3 flex justify-between items-center">
       <div className="flex items-center gap-3">
@@ -11,11 +22,16 @@ const Header = ({ username }: Props) => {
 
       <div className="flex items-center gap-4">
         <input
-          placeholder="Type / to search"
-          className="bg-githubBg border border-githubBorder rounded-md px-3 py-1 text-sm focus:outline-none focus:border-gray-500"
+            placeholder="Type / to search"
+            className="bg-githubBg border border-githubBorder rounded-md px-3 py-1 text-sm focus:outline-none focus:border-gray-500"
         />
-        <div className="w-8 h-8 bg-gray-600 rounded-full"></div>
-      </div>
+
+        <img
+            src={user.avatar_url}
+            alt="profile"
+            className="w-8 h-8 rounded-full object-cover"
+        />
+        </div>
     </div>
   );
 };
